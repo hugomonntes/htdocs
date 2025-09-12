baaa2caa5a8aa9a797f1bdd675b00a8d326fea8b
hugomonntes <hmontes.dev@gmail.com>
2025-09-12 11:15:35 +0200

    Update

--- projects/info.php
+++ projects/info.php
@@ -1,9 +1,20 @@
 <?php // Etiquetas comienzo y final
     echo "<h1>Hola Mundo</h1>";
+
     for ($i=0; $i < 10; $i++) { 
-        echo "${i}";
+        //echo "${i}";
     }
-    $dataString = "";
+
+    $dataString = "uno";
     $dataSetLogin = [1, "Hugo", "h@gmail.com", true];
     $dataSets = array(1, "Hugo", "h@gmail.com", true);
+
+    foreach ($dataSetLogin as $dato) {
+        echo var_dump($dato);
+        echo "${dato}"."<br>";
+    }
+
+    define("CONST", "const");
+
+    // echo "El valor 1 es ". $dataString // Se puede concatenar con el punto .
 ?>
