69e4fa65c5ab50d3a7703aca98536e2852745887
hmontes <hmontes.dev@gmail.com>
2025-10-07 10:56:57 +0200

    Update

--- projects/EXB2_(FORMS_PHP)/datosCostes.php
+++ projects/EXB2_(FORMS_PHP)/datosCostes.php
@@ -1,3 +1,24 @@
 <?php
+// Precio, Cantidad y Descuento son entradas de tipo texto de longitud 5
+// • Iva es una entrada de tipo texto de longitud 3
+// • Método de envío es una entrada de tipo select con los valores
+// o Recoger en tienda ->Coste 0
+// o Correos->Coste 5
+// o Seur->Coste 8
+// El método de envío del formulario es POST
+// El precio total se calcula como ((Precio+Precio*IVA)-descuento)*cantidad+Envío
+// Haz que se muestre por pantalla el mensaje El precio total del envío es…
+
+$campos = ["precio", "cantidad", "descuento", "iva", "envio"];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    foreach ($campos as $campo) {
+
+    }
+}
+
+
+function calcularCostes(){
+
+}
 
 ?>
--- projects/EXB2_(FORMS_PHP)/formCostes.php
+++ projects/EXB2_(FORMS_PHP)/formCostes.php
@@ -11,18 +11,18 @@
     <form action="" method="post">
         <p>Rellena el siguiente formulario para calcular el coste total del envío:</p>
         <label>Precio: </label>
-        <input type="number"><br><br>
+        <input type="number" name="precio"><br><br>
         <label>Cantidad: </label>
-        <input type="number"><br><br>
+        <input type="number" name="cantidad"><br><br>
         <label>Descuento: </label>
-        <input type="number"><br><br>
+        <input type="number" name="descuento"><br><br>
         <label>IVA %</label>
-        <input type="number"><br><br>
+        <input type="number" name="iva"><br><br>
         <label>Método de envío: </label>
         <select name="envio"><br><br>
             <option value="Recoger en tienda">Recoger en tienda</option>
-            <option value="Recoger en tienda">Correos</option>
-            <option value="Recoger en tienda">Seur</option>
+            <option value="Correos">Correos</option>
+            <option value="Seur">Seur</option>
         </select><br><br>
         <input type="submit" value="Calcular Precio Total">
     </form>
