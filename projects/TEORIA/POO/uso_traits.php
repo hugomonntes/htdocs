c9f6c3e20df073e50c831c1b0eb4feac47dc0e97
hmontes <hmontes.dev@gmail.com>
2025-10-14 11:59:49 +0200

    Update

---
+++ projects/TEORIA/POO/uso_traits.php
@@ -0,0 +1,13 @@
+<?php
+    require_once "traits.php";
+
+    class Comunication {
+        use SaludarTrait, DepedirTrait;
+    }
+
+    $obj1 = new Comunication();
+    $obj1->decirHola();
+    echo "<br>";
+    $obj2 = new Comunication();
+    $obj2->sayHello();
+?>
