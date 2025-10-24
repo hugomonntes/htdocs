1a3ab0b7a70873efae5b5a2f4e2a0f0e0457725b
hmontes <hmontes.dev@gmail.com>
2025-10-24 12:15:11 +0200

    Update

---
+++ mvc/modelos/model.php
@@ -0,0 +1,17 @@
+<?php
+class Empleado
+{
+    private $empleado;
+    private $db;
+    public function __construct()
+    {
+        $this->empleado = [];
+        $this->db = new PDO("mysql:host=localhost;dbname=ejemplo_mvc;charset=utf8", "root", "");
+    }
+    public function setEmpleado($nombre, $apellido, $telefono, $departamento)
+    {
+        $query = "INSERT INTO empleados(nombre,apellidos,telefono,departamento) VALUES '$nombre', '$apellido', '$telefono', '$departamento'";
+        $queryResult = $this->db->query($query);
+    }
+}
+?>
