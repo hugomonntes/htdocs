591913ac9986fec0e8c278e79e78114b95aa9e3c
hmontes <hmontes.dev@gmail.com>
2025-11-07 12:20:37 +0100

    Feat(fn): Add all funcionality view and controllers

--- mvc/vistas/vista_editar.php
+++ mvc/vistas/vista_editar.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <h1>Editar Datos</h1>
-    <form action="../controladores/controlador_actualizar.php" method="post">
+    <form action="../controladores/controladorActualizar.php" method="post">
         <input type="text" name="nombre" id="nombre" value="<?php echo $dato[0]['nombre'] ?>">
         <input type="text" name="apellidos" id="apellidos" value="<?php echo $dato[0]['apellidos'] ?>">
         <input type="tel" name="telefono" id="telefono" value="<?php echo $dato[0]['telefono'] ?>">
