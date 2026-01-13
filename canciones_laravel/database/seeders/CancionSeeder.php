dc414f06c2ddb94c30ecee6091f5ec1541dbba13
hmontes <hmontes.dev@gmail.com>
2026-01-13 11:49:28 +0100

    feat(canciones): create CancionSeeder to populate database with sample songs

---
+++ canciones_laravel/database/seeders/CancionSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CancionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('cancions')->insert([
+            [
+                'titulo' => 'Bohemian Rhapsody',
+                'album' => 'A Night at the Opera',
+                'grupo' => 'Queen',
+                'anio' => 1975,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'titulo' => 'Hotel California',
+                'album' => 'Hotel California',
+                'grupo' => 'Eagles',
+                'anio' => 1976,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'titulo' => "Don't Stop Believin'",
+                'album' => "Journey",
+                'grupo' => "Journey",
+                "anio" => 1978,
+                "created_at" => now(),
+                "updated_at" => now()
+            ]
+        ]);
+    }
+}
