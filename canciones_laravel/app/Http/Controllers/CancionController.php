773da934bd5e862f0561478be085f72571070e8d
hmontes <hmontes.dev@gmail.com>
2026-01-20 12:23:17 +0100

    feat(laravel): add fns

--- canciones_laravel/app/Http/Controllers/CancionController.php
+++ canciones_laravel/app/Http/Controllers/CancionController.php
@@ -7,32 +7,37 @@ use App\Models\Cancion;
 
 class CancionController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $songs = Cancion::get();
-        return view('inicio',['canciones'=>$songs]);
+        return view('inicio', ['canciones' => $songs]);
     }
 
-    public function agregar_cancion(Request $peticion){
+    public function agregar_cancion(Request $peticion)
+    {
         $cancion = new Cancion();
         $cancion->titulo = $peticion->titulo;
         $cancion->album = $peticion->album;
         $cancion->grupo = $peticion->grupo;
         $cancion->anio = $peticion->anio;
         $cancion->save();
-        session()->flash('mensaje','Cancion agregada correctamente');
+        session()->flash('mensaje', 'Cancion agregada correctamente');
         return redirect()->route('inicio');
     }
 
-    public function eliminar_cancion(Request $peticion){
-        $ideliminar=$peticion->route('id');
-        $cancion=Cancion::findOrFail($ideliminar);
+    public function eliminar_cancion(Request $peticion)
+    {
+        $ideliminar = $peticion->route('id');
+        $cancion = Cancion::findOrFail($ideliminar);
         $cancion->delete();
-        session()->flash('mensaje','Cancion eliminada correctamente');
+        session()->flash('mensaje', 'Cancion eliminada correctamente');
         return redirect()->route('inicio');
     }
 
-    public function editarCancion(Request $peticion){
-        $idEditar=$peticion->route('id');
-        $cancion=Cancion::findOrFail($idEditar);
+    public function editarCancion(Request $peticion)
+    {
+        $idEditar = $peticion->route('id');
+        $cancion = Cancion::findOrFail($idEditar);
+        return view('editar_cancion', ['cancion' => $cancion]);
     }
 }
