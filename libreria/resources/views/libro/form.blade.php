c225253c7496d784f845ec7f8df8ff4842ad6e17
hmontes <hmontes.dev@gmail.com>
2026-02-03 11:19:19 +0100

    fix(principal): add footer with CRUD information to principal view

--- libreria/resources/views/libro/form.blade.php
+++ libreria/resources/views/libro/form.blade.php
@@ -1,11 +1,13 @@
 <div class="row padding-1 p-1">
     <div class="col-md-12">
-        
-        <div class="form-group mb-2 mb20">
+        {{ Form::label('categoria') }}
+        {{ Form::select('categoria_id',$categorias, $libro->categoria_id, ['class' => 'form-control' . ($errors->has('categoria_id') ? ' is-invalid' : ''), 'placeholder' => 'Categoria Id']) }}
+        {!! $errors->first('categoria_id', '<div class="invalid-feedback">:message</div>') !!}
+        {{-- <div class="form-group mb-2 mb20">
             <label for="categoria_id" class="form-label">{{ __('Categoria Id') }}</label>
             <input type="text" name="categoria_id" class="form-control @error('categoria_id') is-invalid @enderror" value="{{ old('categoria_id', $libro?->categoria_id) }}" id="categoria_id" placeholder="Categoria Id">
             {!! $errors->first('categoria_id', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
-        </div>
+        </div> --}}
         <div class="form-group mb-2 mb20">
             <label for="titulo" class="form-label">{{ __('Titulo') }}</label>
             <input type="text" name="titulo" class="form-control @error('titulo') is-invalid @enderror" value="{{ old('titulo', $libro?->titulo) }}" id="titulo" placeholder="Titulo">
